75df104000677278f4dc37442b6884893e906fc7
McCall Alexander <mccallalexander96@gmail.com>
2016-09-26 11:38:05 -0600

    debugged

--- game.js
+++ game.js
@@ -4,12 +4,13 @@ function Player(username){
     this.name = username;
     this.score = 0;
     this.dropTimes = [Date.now()];
-    this.balloon = new Balloon
+    this.balloon = new Balloon()
 }
 function Balloon(){
     this.altitude = 20;
-    this.sandbags = 5;
-    this.fuel = 100
+    this.goldbars = 5;
+    this.barArr = [""];
+    this.fuel = 100;
     this.lift=0;
     this.weight = 0;
 }
@@ -17,8 +18,9 @@ function burnfuel(a){
     a.lift -= 0.2;
     a.fuel -= 1;
 }
-function removeBag(b){
-    if(b.sandbags > 0){b.sandbags -= 1; b.dropTimes.push(Date.now())}
+function removeBag(p){
+    //player has the drop time
+    if(p.balloon.goldbars > 0){p.balloon.goldbars -= 1; p.dropTimes.push(Date.now())}
 }
 function calculateScore(player){
     var dropTimes = player.dropTimes;
@@ -49,19 +51,28 @@ function gameover(player){
 }
 function update(p){
     var a = p.balloon;
+    if(a.barArr.length < a.goldbars){
+        a.barArr.push("<img src='https://cdn1.iconfinder.com/data/icons/business-colored-icons-vol-1/128/019-512.png'/>")
+    }
+    if(a.barArr.length -2 > a.goldbars){
+        a.barArr.pop();
+    }
     a.lift+= 0.05
-    a.weight = (a.sandbags/5);
+    a.weight = (a.goldbars/5);
     a.altitude += gravity*(a.weight) + a.lift;
     a.altitude = (a.altitude < 0)? Math.abs(a.altitude): a.altitude;
 
     $('#balloon').css('top', a.altitude + "%");
-    $('#bags').html(a.sandbags);
+    $('#bars').html(a.barArr.join(""))
+    // $('#bars').html(a.goldbars);
     $('#health').height(a.fuel + "%");
     var waterlevel = $('.water').offset().top;
     var balloonBottom = $('#balloon').offset().top + $('#balloon').height();
-    if(balloonBottom > waterlevel || a.fuel === 0){
+    if(balloonBottom > waterlevel || a.fuel <= 0){
         gameover(p);
-        timer = 0
+        //Timer is an object that needs to be canceled
+        //look at cancel interval
+        clearInterval(timer)
     }
    
 }
@@ -76,13 +87,12 @@ function keyFunct(){
         if(key == 38){
            burnfuel(balloon)
         }else if(key == 40){
-            removeBag(balloon);
-
+            removeBag(player);
         }
     });
 }
 var timer = setInterval(function(){update(player)}, 100);
-
+debugger
 keyFunct();
 
 
